a21e5dd4e31f29b80688e9b6f48b3da778e65ba6
JakubStacho <stacho.jakub@gmail.com>
2021-04-16 23:02:45 +0200

    Shit water movement

---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{    
+    private EntityMovement entity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        entity = GetComponent<EntityMovement>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var vertical = Input.GetAxisRaw("Vertical");
+        var horizontal = Input.GetAxisRaw("Horizontal");
+
+        entity.SetMoveDirection(new Vector3(horizontal, vertical, 0));
+    }
+}
