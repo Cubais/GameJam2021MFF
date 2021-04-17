5967253ed49930512d1751b9e0ba5d5bcfa0d9d3
Adam Lechovský <lechy901@gmail.com>
2021-04-18 00:57:36 +0200

    Added enemy spawning and trapdoor opening logic to the final level

--- Assets/Resources/Scripts/RoomController.cs
+++ Assets/Resources/Scripts/RoomController.cs
@@ -5,16 +5,50 @@ using UnityEngine;
 public class RoomController : MonoBehaviour
 {
     public List<GameObject> trapdoors;
+    public float rotationSpeed = 10;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private bool m_active;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (m_active && transform.childCount > 0)
+        {
+            StartCoroutine("RotateTrapdoorsCoroutine");
+
+            m_active = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            m_active = true;
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
+    }
+
+    private IEnumerator RotateTrapdoorsCoroutine()
+    {
+        bool iterate = true;
+        while (iterate)
+        {
+            foreach (GameObject trapdoor in trapdoors)
+            {
+                if (trapdoor.transform.eulerAngles.z > -92f)
+                {
+                    trapdoor.transform.eulerAngles = new Vector3(0, 0, trapdoor.transform.eulerAngles.z - rotationSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    iterate = false;
+                }
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
