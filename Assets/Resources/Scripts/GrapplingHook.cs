f8a4fd51d9982cf2dddf5f88fd87b008405e80f8
Adam Lechovský <lechy901@gmail.com>
2021-04-17 13:12:23 +0200

    Added grappling hook

---
+++ Assets/Resources/Scripts/GrapplingHook.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplingHook : MonoBehaviour
+{
+    public float hookSpeed = 10;
+    public float latchSpeed = 10;
+    public float maxDistance = 15;
+    public float minLatchDistance;
+    public Transform maskPivot;
+
+    private Vector2 m_moveDir;
+    private GameObject m_character;
+    private bool m_latched;
+
+    void Start()
+    {
+        m_character = GameObject.Find("Character");
+    }
+
+    void Update()
+    {
+        float playerDistance = Vector3.Distance(transform.position, m_character.transform.position);
+        Vector3 playerDir3 = m_character.transform.position - transform.position;
+        Vector2 playerDir = new Vector2(playerDir3.x, playerDir3.y).normalized;
+
+        if (m_latched)
+        {
+            // Move player toward hook
+            m_character.transform.position += (Vector3)(-playerDir) * latchSpeed * Time.deltaTime;
+            if (playerDistance < minLatchDistance)
+            {
+                Destroy(gameObject);
+            }
+        }
+        else
+        { 
+            // Move hook towards moveDir
+            if (m_moveDir != null)
+            {
+                transform.position += (Vector3)(m_moveDir * hookSpeed * Time.deltaTime);
+            }
+
+            if (playerDistance > maxDistance)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        // Update rotation towards player
+        float zAngle = Vector2.Angle(Vector2.right, playerDir);
+        if (playerDir.y < 0)
+        {
+            zAngle *= -1;
+        }
+
+        transform.eulerAngles = new Vector3(0, 0, zAngle);
+
+        // Update mask scale to hide excess chain
+        maskPivot.localScale = new Vector3(playerDistance / maxDistance, 1, 1);
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 6) // wall
+        {
+            m_latched = true;
+        }
+    }
+
+    public void SetMoveDirection(Vector2 dir)
+    {
+        m_moveDir = dir;
+        Start();
+        Update();
+    }
+
+    public void DeleteIfLatched()
+    {
+        if (m_latched)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Resources/Scripts/PlayerInput.cs
+++ Assets/Resources/Scripts/PlayerInput.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
-{    
-    private EntityMovement entity;
+{
+    public float hookSpeed = 1;
+    public float hookDistance = 10;
+    public GameObject grapplingHook;
+    public Transform grapplingHookParent;
 
     private int playerLevel = 0;
+    private EntityMovement m_entity;
+    private GrapplingHook m_lastHook;
 
     // Start is called before the first frame update
     void Start()
     {
-        entity = GetComponent<EntityMovement>();    
+        m_entity = GetComponent<EntityMovement>();    
     }
 
     // Update is called once per frame
@@ -21,14 +26,49 @@ public class PlayerInput : MonoBehaviour
         var horizontal = Input.GetAxisRaw("Horizontal");
 
         // Cannot go Up if not in the water
-        if (!entity.InWater())
+        if (!m_entity.InWater())
         {
             vertical = Mathf.Clamp(vertical, -1.0f, 0f);            
         }
 
         //Debug.Log("Vertical " + vertical);
 
-        entity.SetMoveDirection(new Vector3(horizontal, vertical, 0), true);
+        m_entity.SetMoveDirection(new Vector3(horizontal, vertical, 0), true);
+
+        // Grappling hook
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 dir3 = mousePosition - transform.position;
+            Vector2 dir = new Vector2(dir3.x, dir3.y).normalized;
+
+            foreach (Transform child in grapplingHookParent)
+            {
+                Destroy(child.gameObject);
+            }
+
+            GameObject hook = Instantiate(grapplingHook, transform.position + (Vector3)dir * 0.3f, Quaternion.identity, grapplingHookParent);
+            m_lastHook = hook.GetComponent<GrapplingHook>();
+            m_lastHook.SetMoveDirection(dir);
+        }
+
+        // If a movement key is pressed, delete latched hook
+        if (
+            Input.GetKey(KeyCode.W) ||
+            Input.GetKey(KeyCode.A) ||
+            Input.GetKey(KeyCode.S) ||
+            Input.GetKey(KeyCode.D) ||
+            Input.GetKey(KeyCode.LeftArrow) ||
+            Input.GetKey(KeyCode.RightArrow) ||
+            Input.GetKey(KeyCode.UpArrow) ||
+            Input.GetKey(KeyCode.DownArrow)
+        )
+        {
+            if (m_lastHook != null)
+            {
+                m_lastHook.DeleteIfLatched();
+            }
+        }
     }
 
     public void LevelUp()
