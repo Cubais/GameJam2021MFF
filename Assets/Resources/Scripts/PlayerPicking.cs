6165205db6e090fbaef91974e139b96454947daf
JakubStacho <stacho.jakub@gmail.com>
2021-04-18 09:53:41 +0200

    Poo2 + Poo1 finished

--- Assets/Resources/Scripts/PlayerPicking.cs
+++ Assets/Resources/Scripts/PlayerPicking.cs
@@ -4,22 +4,11 @@ using UnityEngine;
 
 public class PlayerPicking : MonoBehaviour
 {
-	public GameObject pickupPrefab;
-	public Transform pickupParent;
-
     private PlayerHealth m_PlayerHealth;
 
 	private void Start()
 	{
 		m_PlayerHealth = GetComponent<PlayerHealth>();
-	}
-
-	private void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.Q))
-		{
-			GameObject.Instantiate(pickupPrefab, Camera.main.ScreenToWorldPoint(Input.mousePosition) + Vector3.forward * 10f, Quaternion.identity, pickupParent);
-		}
 	}
 
 	private void OnTriggerEnter2D(Collider2D collision)
